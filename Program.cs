bc7d31c5b8905805a38517a92ab2cb1a5836c205
Azra <yukselazrael@gmail.com>
2024-01-31 20:23:31 +0300

    Added Project

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PolimorfizmExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape;
+
+            //Şekilden türemiş Daire objesi
+            shape = new Daire();
+            shape.Ciz();
+
+            shape = new Kare();
+            shape.Ciz();
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolimorfizmExample
+{
+    internal class Shape
+    {
+        public virtual void Ciz() 
+        {
+            Console.WriteLine("Bir Şekil Çiz");
+        }
+    }
+
+    class Kare:Shape
+    {
+        public override void Ciz()
+        {
+            Console.WriteLine("Kare Çizildi");
+        }
+    }
+
+    class Daire : Shape
+    {
+        public override void Ciz()
+        {
+            Console.WriteLine("Daire Çizildi");
+        }
+    }
+}
